40c75efe7e000679dc31e3f31a58f098dfdacee9
daniel <daniel.molyneaux@queensu.ca>
2019-08-22 14:02:38 -0400

    initial commit;

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import AllArtistMap from './components/allArtistMap';
+
+export class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <AllArtistMap/>
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ client/src/components/allArtistMap.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Plotly from 'plotly.js-mapbox-dist'
+import createPlotlyComponent from 'react-plotly.js/factory';
+const Plot = createPlotlyComponent(Plotly);
+require('dotenv').config() 
+
+const API_KEY = process.env.REACT_APP_MAP_KEY;
+
+
+export default class AllArtistMap extends Component {
+  render() {
+    Plotly.setPlotConfig({
+      mapboxAccessToken: API_KEY 
+    })
+    return (
+        <div justify="center"> 
+            <Plot
+                data={[
+                {
+                  type:'scattermapbox',
+                  lat:['45.5017'],
+                  lon:['-73.5673'],
+                  name: 'Dog',
+                  mode:'markers',
+                  marker: {
+                    size:14
+                  },
+                  text:'Montreal',
+                  hoverinfo:'text'
+                },
+                ]}
+                layout={{width: 2000,
+                         height: 1000,
+                          hovermode:'closest',
+                          mapbox: {
+                            bearing:0,
+                            center: {
+                              lat:45,
+                              lon:-73
+                            },
+                            pitch:0,
+                            zoom:5,
+                            style:'light',
+                            showlegend: true,
+                          }, 
+                          title: 'A Plot'}}
+            />
+      </div>
+    );
+  }
+}
